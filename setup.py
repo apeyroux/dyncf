9508d4b9a3bcb7f60211d7139a6e8a8d1bca1f48
Jean-Alexandre Peyroux <m@ja.pe>
2015-08-19 13:20:43 +0200

    init repo

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='dyncf',
+    version='0.1',
+    packages=[''],
+    url='https://gist.github.com/j4/79792a50e8f4b534b99f',
+    license='',
+    author='ja',
+    author_email='m@ja.Pe',
+    description='Mini DynDNS pour CloudFlare',
+    scripts=['dyncf']
+)
